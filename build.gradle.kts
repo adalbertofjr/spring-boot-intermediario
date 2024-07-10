9f5a0d2dfdb310da1c39bfeeccef822fad5cb203
Adalberto Fernandes Júnior <adalberto.fernandesjr@gmail.com>
2024-07-10 20:19:28 -0300

    conectando o banco de dados

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java:8.0.33")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.springfox:springfox-swagger2:3.0.0")
 	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("javax.validation:validation-api:2.0.1.Final")
 	implementation("org.hibernate:hibernate-validator:8.0.1.Final")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
